c00cd8a4dc9628ef2e494fe3c3d73fbea7245a6a
philypevinicius <philype20@hotmail.com>
2023-03-14 12:37:38 -0300

    inicio do projeto

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Evento from './components/Evento';
+import Form from './components/Form';
+
+function App() {
+  const nome = 'Maria'
+
+  return (
+    <div className="App">
+      <h1>Testando Eventos</h1>
+      <Evento numero="1" />
+      <Evento numero="2" />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
